2c0bf123dcb6d8806c2dbe34e16ec5a1fdb7bd3c
StefPY <stefan.mail.sk@gmail.com>
2019-10-16 12:43:51 +0200

    Game works, to add already made moves

--- Board.py
+++ Board.py
@@ -1,10 +1,14 @@
-class PseudoBoard():
+class Board():
 
     board = list(("-") for x in range(9))
 
     def __init__(self, ox):
         self.ox = ox.upper()
 
+    def update_board(self, pos, ox):
+        self.board.pop(pos)
+        self.board.insert(pos, ox)
+        return self.board
 
     def player(self, pos):
         """Returns a new board in list format after players move"""
@@ -13,28 +17,28 @@ class PseudoBoard():
             self.board.insert(pos-1, self.ox)
         return self.board
 
-    def checkH(self):
+    def check_h(self, player):
         """Check HORIZONTALLY whether win row is available"""
-        first  = all((self.ox == x for x in self.board[0:3])) #1,2,3
-        second = all((self.ox == x for x in self.board[3:6])) #4,5,6
-        third = all((self.ox == x for x in self.board[6:9]))  #7,8,9
+        first  = all((player == x for x in self.board[0:3])) #1,2,3
+        second = all((player == x for x in self.board[3:6])) #4,5,6
+        third = all((player == x for x in self.board[6:9]))  #7,8,9
         return any([first, second, third])
 
-    def checkV(self):
+    def check_v(self, player):
         """Check VERTICALLY whether win row is available"""
-        first  = all((self.ox == x for x in self.board[0:7:3])) #1,4,7
-        second = all((self.ox == x for x in self.board[1:8:3])) #2,5,8
-        third = all((self.ox == x for x in self.board[2:9:3]))  #3,6,9
+        first  = all((player == x for x in self.board[0:7:3])) #1,4,7
+        second = all((player == x for x in self.board[1:8:3])) #2,5,8
+        third = all((player == x for x in self.board[2:9:3]))  #3,6,9
         return any([first, second, third])
 
 
-    def checkDiagR(self):
+    def check_diag_r(self, player):
         """Check whether in RIGHT DIAGONAL win row is available"""
-        return all((self.ox == self.board[x] for x in [0, 4, 8])) # 1, 5, 9
+        return all((player == self.board[x] for x in [0, 4, 8])) # 1, 5, 9
 
-    def checkDiagL(self):
+    def check_diag_l(self,player):
         """Check whether in LEFT DIAGONAL win row is available"""
-        return all((self.ox == self.board[x] for x in [2, 4, 6])) # 3, 5, 7
+        return all((player == self.board[x] for x in [2, 4, 6])) # 3, 5, 7
 
     def show(self):
         """CLI 3x3 visual output"""
@@ -46,21 +50,9 @@ class PseudoBoard():
                 print("...................")
         print(2*"\n")
 
+    def answer_to_move(self, move, player):
+        return self.board.insert(move, player)
+
     def __str__(self):
         """API format output of state"""
         return "".join(self.board)
-
-
-
-board = PseudoBoard("X")
-board.player(7)
-board.player(8)
-board.player(5)
-print(f"Horizontal: {board.checkH()}")
-print(f"Vertical:   {board.checkV()}")
-print(f"Diagonal L: {board.checkDiagL()}")
-print(f"Diagonal R: {board.checkDiagR()}")
-board.show()
-
-
-
---
+++ CLI main.py
@@ -0,0 +1,96 @@
+import requests
+from Board import Board
+
+def request_move(move, player):
+    url = f"https://stujo-tic-tac-toe-stujo-v1.p.rapidapi.com/{move}/{player}"
+    headers = {
+    'x-rapidapi-host': "stujo-tic-tac-toe-stujo-v1.p.rapidapi.com",
+    'x-rapidapi-key': "3efad19192msh6a98b6713edaf48p177302jsn3793168952fd"
+    }
+
+    response = requests.request("GET", url, headers=headers, )
+    return response.json()
+
+def win_check(board, player):
+    winner = []
+    if board.check_h(player):
+        winner = ['HORIZONTAL', player]
+        return winner
+    if board.check_v(player):
+        winner = ['VERTICAL', player]
+        return winner
+    if board.check_diag_r(player):
+        winner = ['RIGHT DIAGONAL', player]
+        return winner
+    if board.check_diag_l(player):
+        winner = ['LEFT DIAGONAL', player]
+        return winner
+    return False
+
+def main():
+
+    PLAYER =""
+    print("W E L C O M E  T O  T H E  G A M E")
+    print("==================================")
+    print("            TIC TAC TOE           ")
+    print(" by Stefan                  2019  ")
+    print()
+
+    while PLAYER not in ['x', 'X','o','O']:
+        PLAYER = input('Chooose X or O : ').upper()
+    if PLAYER == "X":
+        api_player = "O"
+    else:
+        api_player = "X"
+    board = Board(PLAYER)
+
+    print()
+    print("This is the playing board:")
+    board.show()
+    while True:
+        p_move = int(input('Chooose position [1-9]: '))
+        if str(p_move) in '123456789':
+            print()
+            board.player(p_move)
+            board.show()
+            win = win_check(board, PLAYER)
+            if win:
+                print("     C O N G R A T U L A T I O N S  !!!")
+                print("--------------------------------------------")
+                print(f'       YOU WON in {win[0]} LINE.')
+                print()
+                print('          G A M E  O V E R  !!!        ')
+                break
+            elif '-' not in board.board:
+                print("        N O  W I N N E R  ! ! !     ")
+                print("----------------------------------------")
+                print()
+                print('     G A M E  O V E R  !!!        ')
+                break
+
+            X = request_move(board, PLAYER)
+            board.update_board(X['recommendation'], api_player)
+            print(f"Strenght of opponents move is [{X['strength']}]")
+            board.show()
+            lost = win_check(board, player=api_player)
+            if lost:
+                print("   W H A T  A  S H A M E  !!!")
+                print("--------------------------------")
+                print('You lost against the API.')
+                print(f'API won with {lost[0]} LINE.')
+                print()
+                print('     G A M E  O V E R  !!!        ')
+                break
+            elif '-' not in board.board:
+                print("        N O  W I N N E R  ! ! !     ")
+                print("----------------------------------------")
+                print()
+                print('     G A M E  O V E R  !!!        ')
+                break
+
+
+if __name__ == '__main__':
+    main()
+
+
+

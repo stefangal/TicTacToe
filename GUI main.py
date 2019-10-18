749b7895451da50f25dda1801c6fb0319cd7ad28
StefPY <stefan.mail.sk@gmail.com>
2019-10-18 14:30:37 +0200

    GUI click command added

--- GUI main.py
+++ GUI main.py
@@ -29,29 +29,73 @@ photo_o = ImageTk.PhotoImage(image_o)
 photo_x = ImageTk.PhotoImage(image_x)
 photo_e = ImageTk.PhotoImage(image_e)
 
-def clicked(pos):
-    bt = 'button'+str(pos)
-    ph = 'photo_x'
-    return globals()[bt].config(image=globals()[ph])
+global x
+x = 1
+
+def click1():
+    global x
+    lst = [photo_x, photo_o]
+    return button1.config(image=lst[x],width=70, height=70)
+
+def click2():
+    global x
+    lst = [photo_x, photo_o]
+    return button2.config(image=lst[x],width=70, height=70)
+
+def click3():
+    global x
+    lst = [photo_x, photo_o]
+    return button3.config(image=lst[x],width=70, height=70)
+
+def click4():
+    global x
+    lst = [photo_x, photo_o]
+    return button4.config(image=lst[x],width=70, height=70)
+
+def click5():
+    global x
+    lst = [photo_x, photo_o]
+    return button5.config(image=lst[x],width=70, height=70)
+
+def click6():
+    global x
+    lst = [photo_x, photo_o]
+    return button6.config(image=lst[x],width=70, height=70)
+
+def click7():
+    global x
+    lst = [photo_x, photo_o]
+    return button7.config(image=lst[x],width=70, height=70)
+
+def click8():
+    global x
+    lst = [photo_x, photo_o]
+    return button8.config(image=lst[x],width=70, height=70)
+
+def click9():
+    global x
+    lst = [photo_x, photo_o]
+    return button9.config(image=lst[x],width=70, height=70)
+
 
 # 1 - 9 BUTTONS
-button1 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button1 = tk.Button(frame19, command=click1, image=photo_e, width=70, height=70, bg='white', fg='red')
 button1.grid(column=0, row=0)
-button2 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button2 = tk.Button(frame19, command=click2, image=photo_e, bg='white', fg='red')
 button2.grid(column=1, row=0)
-button3 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
-button3.grid(column=2, row=0, sticky='E')
-button4 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button3 = tk.Button(frame19, command=click3, image=photo_e, bg='white', fg='red')
+button3.grid(column=2, row=0)
+button4 = tk.Button(frame19, command=click4, image=photo_e, bg='white', fg='red')
 button4.grid(column=0, row=1)
-button5 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button5 = tk.Button(frame19, command=click5, image=photo_e, bg='white', fg='red')
 button5.grid(column=1, row=1)
-button6 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button6 = tk.Button(frame19, command=click6, image=photo_e, bg='white', fg='red')
 button6.grid(column=2, row=1)
-button7 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button7 = tk.Button(frame19, command=click7, image=photo_e, bg='white', fg='red')
 button7.grid(column=0, row=2)
-button8 = tk.Button(frame19, image=photo_e, bg='white', fg='red')
+button8 = tk.Button(frame19, command=click8, image=photo_e, bg='white', fg='red')
 button8.grid(column=1, row=2)
-button9 = tk.Button(frame19, image=photo_e, command=clicked, bg='white', fg='red')
+button9 = tk.Button(frame19, command=click9, image=photo_e,bg='white', fg='red')
 button9.grid(column=2, row=2)
 
 #  X - O BUTTONS
@@ -63,13 +107,13 @@ buttonO.grid(column=5, row=0)
 
 
 #MARKINGS
-def markings(pos, xo):
-    bt = 'button'+pos
-    ph = 'photo_'+xo
-    return globals()[bt].config(image=globals()[ph])
+# def markings(pos, xo):
+#     bt = 'button'+pos
+#     ph = 'photo_'+xo
+#     return globals()[bt].config(image=globals()[ph])
 
-markings('3', 'x')
-markings('2', 'x')
+# markings('3', 'x')
+# markings('2', 'x')
 
 
 root.mainloop()
